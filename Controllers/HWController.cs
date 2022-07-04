f02c9ad7f2e7fc46699ad8d4b1b3fa49641e3174
柯冠宇 <qazwsxedc59764@gmail.com>
2022-07-04 19:09:26 +0800

    0704/19.09

--- Controllers/HWController.cs
+++ Controllers/HWController.cs
@@ -2,6 +2,7 @@
 using MyHw_Ajax.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +22,16 @@ namespace MyHw_Ajax.Controllers
         public IActionResult HW2()
         {
             return View();
+        }
+        public ContentResult Check(string name)
+        {
+            var q = from m in db.Members where m.Name == name select m;
+            if (q.Count() > 0) return Content($"已有名字為{name}的會員", "text/plain", System.Text.Encoding.UTF8);
+            else
+            {
+                return Content($"{name}可使用", "text/plain", System.Text.Encoding.UTF8);
+            }
+
         }
     }
 }
